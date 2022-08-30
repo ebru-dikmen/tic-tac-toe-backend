893c2f63c6186e8e9423bb967345e52c0bf167f2
ebru-dikmen <ebrudikmen00@gmail.com>
2022-08-30 03:05:07 +0300

    config is created.

---
+++ config.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcryptjs');
+
+const config = {
+    salt: '$2a$12$Cyf/.qgGzFFK1lkACJQWb.', // bcrypt.genSaltSync(12)
+    secret: '12345!ebru.6789',
+    expire: 1800 // 30 minute
+};
+
+module.exports = config;
